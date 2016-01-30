5c8922892b93070339a47cbad919fee1afe973fe
kaustuv deolal <vutsuak96@gmail.com>
2016-01-30 18:18:09 +0530

    redundant import removed

--- tweety.py
+++ tweety.py
@@ -1,6 +1,6 @@
 __name__ = "vutsuak"
 
-import tweepy, time
+import tweepy
 
 ACCESS_TOKEN = '2309050262-pkZPWvaA8PxSqqyXYPrAVgEFNJKx3rEJjMTafzZ'
 ACCESS_SECRET = 'U2XKNahEE38dO8oTNRe2Ms6n569SYIoCPiShg8GiqCzE7'
@@ -18,12 +18,15 @@ for user in tweepy.Cursor(api.friends, screen_name="kaustuv deolal").items():
     following.append((str(user.screen_name)))
 
 
-ct = 5
+ct = 5 #only for 5 tweets
 for tweet in tweepy.Cursor(api.search, q=keyword).items():
     if ct == 0:
         break
-    if tweet.user.screen_name not in following:
-            api.create_friendship(tweet.user.id)
-            api.create_favorite(tweet.id)
+    try:
+        if tweet.user.screen_name not in following:
+                api.create_friendship(tweet.user.id)
+                api.create_favorite(tweet.id)
+    except:
+        pass
 
     ct -= 1
