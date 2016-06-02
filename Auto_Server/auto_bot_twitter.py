31fae741e9b6a8fbc5ebc54bb9a5357efbcf8952
kaustuv deolal <vutsuak96@gmail.com>
2016-06-02 17:39:15 +0530

    time check

--- Auto_Server/auto_bot_twitter.py
+++ Auto_Server/auto_bot_twitter.py
@@ -1,17 +1,36 @@
 from flask import Flask, request, redirect, url_for, render_template
 import tweepy
-
+import datetime
+import time
+import json
 
 app = Flask(__name__)
 ACCESS_TOKEN = '2309050262-pkZPWvaA8PxSqqyXYPrAVgEFNJKx3rEJjMTafzZ'
 ACCESS_SECRET = 'U2XKNahEE38dO8oTNRe2Ms6n569SYIoCPiShg8GiqCzE7'
 CONSUMER_KEY = 'vCU2EhCs53ti6Vph6qcThv6Zy'
 CONSUMER_SECRET = 'SXtnxlNA0GAHDaLRfvEGHOXbpUxTD6i0sYFTTa972glBBmEOQF'
-
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
-
 api = tweepy.API(auth)
+
+
+class StdOutListener(tweepy.StreamListener):
+    def __init__(self):
+        self.limit = 25
+        self.number_of_tweets = 0
+
+    def on_data(self, status):
+        self.number_of_tweets += 1
+        print  json.loads(status)['created_at'].split(" ")[3]
+        print json.loads(status)['text']
+        if not (self.number_of_tweets < self.limit):
+            print "LIMIT REACHED"
+            return False
+        else:
+            return True
+
+    def on_error(self, status):
+        print status
 
 
 @app.route("/twitter_bot", methods=["GET", "POST"])
@@ -21,11 +40,25 @@ def twitter_bot():
     else:
         if request.form["optradio"] == "follow":
             key = request.form["key"]
+            follow_like(key)
 
         elif request.form["optradio"] == "UnFollow":
-            key = request.form["key"]
+            UnFollow()
 
         return redirect('/twitter_bot')
+
+
+def follow_like(key):
+    l = StdOutListener()
+    stream = tweepy.Stream(auth, l)
+    stream.filter(track=[key])
+
+
+def UnFollow():
+    for user in tweepy.Cursor(api.friends, screen_name='kaustuv deolal').items():
+        print user.screen_name
+        print user.created_at
+        print "\n"
 
 
 if __name__ == "__main__":
